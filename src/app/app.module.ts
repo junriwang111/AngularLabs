bbacc15facfa0b2cbff4cfdaceef76ffcf411a3d
junri wang <junri.wang88658@gmail.com>
2020-08-28 12:59:28 +1000

    week5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,20 @@ import { AppComponent } from "./app.component";
 import { LoginComponent } from "./login/login.component";
 import { AccountComponent } from "./account/account.component";
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+
+import { ProfileComponent } from "./profile/profile.component";
+import { LogoutComponent } from "./logout/logout.component";
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, AccountComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    AccountComponent,
+    ProfileComponent,
+    LogoutComponent
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { FormsModule } from "@angular/forms";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: "app-login",
@@ -8,37 +9,47 @@ import { FormsModule } from "@angular/forms";
   styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
-  users = [
-    { email: "abc@com.au", pwd: "123" },
-    { email: "abd@com.au", pwd: "123" },
-    { email: "abe@com.au", pwd: "123" }
-  ];
+  // users = [
+  //   { email: "abc@com.au", pwd: "123" },
+  //   { email: "abd@com.au", pwd: "123" },
+  //   { email: "abe@com.au", pwd: "123" }
+  // ];
 
   email: string;
   password: string;
   showmessage = false;
   hidemessage = true;
+  uri: string = "http://localhost:3000";
+  // http: any;
 
-  constructor(private router: Router, private form: FormsModule) {}
+  constructor(
+    private router: Router,
+    private form: FormsModule,
+    private http: HttpClient
+  ) {}
 
   ngOnInit() {}
 
   itemClicked() {
-    var match = false;
-    for (let i = 0; i < this.users.length; i++) {
-      if (
-        this.email == this.users[i].email &&
-        this.password == this.users[i].pwd
-      ) {
-        this.router.navigateByUrl("/account/" + this.email);
-        // this.router.navigateByUrl("/account");
-        match = true;
-      }
-    }
+    const obj = {
+      email: this.email,
+      upwd: this.password
+    };
 
-    if (!match) {
-      this.showmessage = true;
-      this.hidemessage = false;
-    }
+    // this.http.get(`${this.uri}/api/login/${this.email}&${this.password}`).
+    this.http.post(`${this.uri}/api/auth`, obj).subscribe(data => {
+      if (Object.keys(data).length === 0 && data.constructor === Object) {
+        this.showmessage = true;
+        this.hidemessage = false;
+      } else {
+        // this.aaa = data;
+        console.log(data);
+        sessionStorage.setItem("user", JSON.stringify(data));
+        // this.router.navigateByUrl(
+        //   "/account/" + encodeURIComponent(JSON.stringify(data))
+        // );
+        this.router.navigateByUrl("/profile");
+      }
+    });
   }
 }
